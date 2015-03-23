f24e4f5914afd215951b712201e6f0a34edcb77c
Jonathan Ni <***@gmail.com>
2015-03-23 12:35:21 -0400

    Initial Commit

---
+++ 3DPlatformer/3DPlatformer.cpp
@@ -0,0 +1,13 @@
+// 3DPlatformer.cpp : Defines the entry point for the application.
+//
+
+#ifdef _WIN32
+#include "stdafx.h"
+#endif
+
+#include "3DPlatformer.h"
+
+int main(int argc, char * argv[]){
+
+	return 0;
+}
