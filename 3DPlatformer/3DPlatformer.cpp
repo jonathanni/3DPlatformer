87ab32a5fec70e904b65c0f47e60838daccde7f8
Jonathan Ni <***@gmail.com>
2015-03-23 21:40:31 -0400

    Irrlicht Setup

--- 3DPlatformer/3DPlatformer.cpp
+++ 3DPlatformer/3DPlatformer.cpp
@@ -5,9 +5,120 @@
 #include "stdafx.h"
 #endif
 
+#include <iostream>
+#include <irrlicht.h>
+
 #include "3DPlatformer.h"
 
-int main(int argc, char * argv[]){
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
 
+namespace Platformer
+{
+	// Irrlicht namespaces
+	using namespace irr;
+	using namespace core;
+	using namespace scene;
+	using namespace video;
+	using namespace io;
+	using namespace gui;
+
+	Platformer::Platformer()
+	{
+		device =
+			irr::createDevice(irr::video::EDT_SOFTWARE, irr::core::dimension2d<irr::u32>(800, 600), 16,
+			false, false, false, NULL);
+
+		if (!device)
+			success = false;
+	}
+
+	Platformer::~Platformer()
+	{
+		if (success){
+			device->closeDevice();
+			device->run();
+			device->drop();
+		}
+	}
+
+	void Platformer::init(void)
+	{
+		device->setWindowCaption(L"3D Platformer");
+
+		driver = device->getVideoDriver();
+		smgr = device->getSceneManager();
+		guienv = device->getGUIEnvironment();
+
+		guienv->addStaticText(L"Lorem ipsum dolor sit amet.",
+			rect<s32>(10, 10, 260, 22), true);
+
+		testMesh = smgr->getMesh("testMesh.x");
+		if (!testMesh)
+		{
+			device->drop();
+			success = false;
+		}
+		IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(testMesh);
+
+		if (node)
+		{
+			node->setMaterialFlag(EMF_LIGHTING, false);
+			node->setMD2Animation(scene::EMAT_STAND);
+			node->setMaterialTexture(0, driver->getTexture("dirt.png"));
+		}
+
+		smgr->addCameraSceneNode(0, vector3df(2, 2, 2), vector3df(-1, -1, -1));
+	}
+
+	void Platformer::run()
+	{
+		while (device->run())
+		{
+			driver->beginScene(true, true, SColor(255, 100, 101, 140));
+
+			smgr->drawAll();
+			guienv->drawAll();
+
+			driver->endScene();
+		}
+	}
+
+	bool check(Platformer * p)
+	{
+		if (!p->success)
+		{
+			// failure
+			delete p;
+			return false;
+		}
+
+		return true;
+	}
+}
+
+int main(int argc, char * argv[])
+{
+	Platformer::Platformer * pMain = new Platformer::Platformer();
+
+	if (!check(pMain))
+	{ 
+		std::cerr << "Failed creation." << std::endl;
+		return 2;
+	}
+
+	pMain->init();
+
+	if (!check(pMain))
+	{
+		std::cerr << "Failed initialization." << std::endl;
+		return 3;
+	}
+
+	pMain->run();
+
+	delete pMain;
 	return 0;
 }
--- 3DPlatformer/3DPlatformer.h
+++ 3DPlatformer/3DPlatformer.h
@@ -1,1 +1,36 @@
+#ifndef _3DPLATFORMER_H
+#define _3DPLATFORMER_H
 
+namespace Platformer{
+
+	// Irrlicht namespaces
+	using namespace irr;
+	using namespace core;
+	using namespace scene;
+	using namespace video;
+	using namespace io;
+	using namespace gui;
+
+	class Platformer{
+	public:
+		Platformer(void);
+		~Platformer(void);
+
+		void init(void);
+		void run(void);
+
+		IrrlichtDevice * device;
+
+		bool success = true;
+	private:
+		IVideoDriver* driver;
+		ISceneManager* smgr;
+		IGUIEnvironment* guienv;
+
+		IAnimatedMesh* testMesh;
+	};
+
+	bool check(Platformer *);
+}
+
+#endif
